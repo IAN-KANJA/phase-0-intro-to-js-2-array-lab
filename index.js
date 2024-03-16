1bb2794a88004113bcad10e278bb3f66996e3df0
iannjau <lantanacreations@gmail.com>
2024-03-16 09:25:15 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution here!
+function ( Milo ,Otis, Garfield){
+  cat . length =0;
+
+}
+describe('destructivelyAppendCat(name)'), function () {
+    function (); Milo ,Otis, Garfield;
+}
